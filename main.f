6ac4883f25405da69a6eec565e254e8490cd7b13
asnowmang <wsobs@yandex.com>
2018-08-14 03:36:12 -0500

    Switched to logical variables

--- main.f
+++ main.f
@@ -1,5 +1,6 @@
       PROGRAM BUBBLESORT
-      INTEGER n, sorted, tmp
+      LOGICAL sorted
+      INTEGER n, tmp
       INTEGER t(10)
 
 c     Parses file into array
@@ -10,14 +11,14 @@ c     Parses file into array
       CLOSE (log)
 
 c     Sort
-      DO WHILE (sorted .EQ. 0)
-         sorted = 1
+      DO WHILE (sorted .EQV. .FALSE.)
+         sorted = .TRUE.
          DO 20 n = 1, 9
             IF (t(n) .GT. t(n + 1)) THEN
                tmp = t(n)
                t(n) = t(n + 1)
                t(n + 1) = tmp
-               sorted = 0
+               sorted = .FALSE.
             ELSE
                CONTINUE
             ENDIF
